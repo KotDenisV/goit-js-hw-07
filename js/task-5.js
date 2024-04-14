6a13bd973bc3b2990cec3d4c3a84e39e593a3cf2
KotDenisV <kot.denis@gmail.com>
2024-04-15 00:03:43 +0300

    15/04 0:03

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const setColor = document.querySelector('body');
+const span = document.querySelector('.color');
+
+const Click = (event) => {
+  const colorBody = getRandomHexColor();
+  setColor.style.backgroundColor = colorBody;
+  span.textContent = colorBody;
+  
+};
+
+btn.addEventListener("click", Click);
+
